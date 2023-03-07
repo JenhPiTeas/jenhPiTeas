1b0965cad5674e0cd7c7219aad7075fd75b58377
Tan Bunchhay <87403363+Parameow3@users.noreply.github.com>
2023-03-07 13:09:57 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,9 +7,9 @@
     <title>My Contact</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
     <!-- Style -->
-    <link href="{{asset('public/assets/bootstrap-icons/font/bootstrap-icons.css')}}" rel="stylesheet">
-    <link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('public/assets/css/custom.css')}}" rel="stylesheet">
+    <link href="{{asset('../../../public/assets/bootstrap-icons/font/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('../../../public/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('../../../public/assets/css/custom.css')}}" rel="stylesheet">
 </head>
 <body>
 <!-- navbar -->
